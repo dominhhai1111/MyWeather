a5234ba4291c796b36309f16c85ea46ce615fdff
Hai Do Minh <hai.do@ntq-solution.com.vn>
2019-09-03 17:55:29 +0700

    update

--- App.js
+++ App.js
@@ -1,13 +1,37 @@
 import React from 'react';
-import { Platform, StyleSheet, Text, View } from 'react-native';
+import { 
+  Platform, 
+  StyleSheet, 
+  Text, 
+  KeyboardAvoidingView,
+  ImageBackground,
+  View,
+} from 'react-native';
+
+import getImageForWeather from './utils/getImageForWeather';
+
+import SearchInput from './components/SearchInput';
 
 export default function App() {
+  const location = 'Ha Noi';
+
   return (
-    <View style={styles.container}>
-      <Text style={[styles.largeText, styles.textStyle]}>Ha Noi</Text>
-      <Text style={[styles.smallText, styles.textStyle]}>Hot Sunny</Text>
-      <Text style={[styles.largeText, styles.textStyle]}>36°</Text>
-    </View>
+    <KeyboardAvoidingView style={styles.container}>
+      <ImageBackground
+        source={getImageForWeather('Clear')}
+        style={styles.imageContainer}
+        imageStyle={styles.image}
+      >
+        <View style={styles.detailsContainer}>
+          <Text style={[styles.largeText, styles.textStyle]}>{location}</Text>
+          <Text style={[styles.smallText, styles.textStyle]}>Hot Sunny</Text>
+          <Text style={[styles.largeText, styles.textStyle]}>36°</Text>
+
+          <SearchInput placeholder="Search any city"/>
+        </View>
+      </ImageBackground>
+      
+    </KeyboardAvoidingView>
   );
 }
 
@@ -15,17 +39,31 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
+  },
+  detailsContainer: {
+    flex: 1,
     justifyContent: 'center',
+    backgroundColor: 'rgba(0,0,0,0.2)',
+    paddingHorizontal: 20,
   },
   textStyle: {
     textAlign: 'center',
     fontFamily: Platform.OS === 'ios' ? 'AvenirNext-Regular' : 'Roboto',
+    color: 'white',
   },
   largeText: {
     fontSize: 44,
   },
   smallText: {
     fontSize: 18,
+  },
+  imageContainer: {
+    flex: 1,
+  },
+  image: {
+    flex: 1,
+    width: null,
+    height: null,
+    resizeMode: 'cover',
   },
 });
